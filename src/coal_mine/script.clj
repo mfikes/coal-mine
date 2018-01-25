b4cffcd1674749cefbb8ed2eecb392fa2bceec64
Mike Fikes <mike@fikesfarm.com>
2018-01-25 15:49:23 -0500

    wip

---
+++ src/coal_mine/script.clj
@@ -0,0 +1,43 @@
+(ns coal-mine.script
+  (:refer-clojure :exclude [test])
+  (:require
+   [cljs.build.api]
+   [clojure.java.shell :as shell]))
+
+(defn build [source main]
+  (let [progress-thread (doto
+                         (Thread. #(while (try (Thread/sleep 500000) true
+                                               (catch Throwable _ false))
+                                     (println "Still building" main "...")))
+                          .start)]
+    (try
+      (println "\nBuilding" main "...")
+      (cljs.build.api/build source
+        {:optimizations  :none
+         :parallel-build true
+         :target         :nodejs
+         :main           main
+         :output-to      "/tmp/coal-main.js"})
+      (finally (.interrupt progress-thread)))))
+
+(defn test-part [part]
+  (build (str "src/coal_mine/test_runner_" part ".cljs") (symbol (str "coal-mine.test-runner-" part)))
+  (println "Running" (str "coal-mine.test-runner-" part) "in Node ...")
+  (let [results (shell/sh "node" "-max-old-space-size=3072" "/tmp/coal-main.js")]
+    (println (:out results))
+    (println (:err results))
+    (if-not (and (zero? (:exit results))
+                 (re-find #"0 failures, 0 errors." (:out results)))
+      (throw (ex-info "Tests failed" {}))
+      (map #(Long/parseLong %) (rest (first (re-seq #"Ran (\d+) tests containing (\d+) assertions." (:out results))))))))
+
+(defn test []
+  (let [subtotals (doall (map test-part [1 2]))]
+    (prn 'subtotals subtotals)
+    (println "Ran a total of" (apply + (map first subtotals))
+      "tests containing" (apply + (map second subtotals)) "assertions.")))
+
+(defn -main [fun & args]
+  (case fun
+    "test-part" (test-part (Long/parseLong (first args)))
+    "test" (test)))
