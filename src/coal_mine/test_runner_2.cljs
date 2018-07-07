351607d35b3bd97469ed236a97c2ff456aadd11a
Mike Fikes <mike@fikesfarm.com>
2018-07-07 10:34:50 -0400

    Rebalance (#74)

--- src/coal_mine/test_runner_2.cljs
+++ src/coal_mine/test_runner_2.cljs
@@ -1,29 +1,6 @@
 (ns coal-mine.test-runner-2
   (:require
    [clojure.test]
-   coal-mine.problem-33
-   coal-mine.problem-34
-   coal-mine.problem-35
-   coal-mine.problem-36
-   coal-mine.problem-37
-   coal-mine.problem-38
-   coal-mine.problem-39
-   coal-mine.problem-40
-   coal-mine.problem-41
-   coal-mine.problem-42
-   coal-mine.problem-43
-   coal-mine.problem-44
-   coal-mine.problem-45
-   coal-mine.problem-46
-   coal-mine.problem-47
-   coal-mine.problem-48
-   coal-mine.problem-49
-   coal-mine.problem-50
-   coal-mine.problem-51
-   coal-mine.problem-52
-   coal-mine.problem-53
-   coal-mine.problem-54
-   coal-mine.problem-55
    coal-mine.problem-56
    coal-mine.problem-57
    coal-mine.problem-58
@@ -32,33 +9,19 @@
    coal-mine.problem-61
    coal-mine.problem-62
    coal-mine.problem-63
-   coal-mine.problem-64))
+   coal-mine.problem-64
+   coal-mine.problem-65
+   coal-mine.problem-66
+   coal-mine.problem-67
+   coal-mine.problem-68
+   coal-mine.problem-69
+   coal-mine.problem-70
+   coal-mine.problem-71
+   coal-mine.problem-72
+   coal-mine.problem-73))
 
 (defn run-tests []
   (clojure.test/run-tests
-    'coal-mine.problem-33
-    'coal-mine.problem-34
-    'coal-mine.problem-35
-    'coal-mine.problem-36
-    'coal-mine.problem-37
-    'coal-mine.problem-38
-    'coal-mine.problem-39
-    'coal-mine.problem-40
-    'coal-mine.problem-41
-    'coal-mine.problem-42
-    'coal-mine.problem-43
-    'coal-mine.problem-44
-    'coal-mine.problem-45
-    'coal-mine.problem-46
-    'coal-mine.problem-47
-    'coal-mine.problem-48
-    'coal-mine.problem-49
-    'coal-mine.problem-50
-    'coal-mine.problem-51
-    'coal-mine.problem-52
-    'coal-mine.problem-53
-    'coal-mine.problem-54
-    'coal-mine.problem-55
     'coal-mine.problem-56
     'coal-mine.problem-57
     'coal-mine.problem-58
@@ -67,7 +30,16 @@
     'coal-mine.problem-61
     'coal-mine.problem-62
     'coal-mine.problem-63
-    'coal-mine.problem-64))
+    'coal-mine.problem-64
+    'coal-mine.problem-65
+    'coal-mine.problem-66
+    'coal-mine.problem-67
+    'coal-mine.problem-68
+    'coal-mine.problem-69
+    'coal-mine.problem-70
+    'coal-mine.problem-71
+    'coal-mine.problem-72
+    'coal-mine.problem-73))
 
 (defn -main []
   (run-tests))
