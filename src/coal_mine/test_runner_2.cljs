b39d0c8be6b36f21384fada4e46001aa89fae93b
Mike Fikes <mike@fikesfarm.com>
2018-07-15 09:39:06 -0400

    Rebalance

--- src/coal_mine/test_runner_2.cljs
+++ src/coal_mine/test_runner_2.cljs
@@ -2,45 +2,33 @@
   (:require
    [cljs.core.specs.alpha]
    [clojure.test]
-   coal-mine.problem-56
-   coal-mine.problem-57
-   coal-mine.problem-58
-   coal-mine.problem-59
-   coal-mine.problem-60
-   coal-mine.problem-61
-   coal-mine.problem-62
-   coal-mine.problem-63
-   coal-mine.problem-64
-   coal-mine.problem-65
-   coal-mine.problem-66
-   coal-mine.problem-67
-   coal-mine.problem-68
-   coal-mine.problem-69
-   coal-mine.problem-70
-   coal-mine.problem-71
-   coal-mine.problem-72
-   coal-mine.problem-73))
+   coal-mine.problem-74
+   coal-mine.problem-75
+   coal-mine.problem-76
+   coal-mine.problem-77
+   coal-mine.problem-78
+   coal-mine.problem-79
+   coal-mine.problem-80
+   coal-mine.problem-81
+   coal-mine.problem-82
+   coal-mine.problem-83
+   coal-mine.problem-84
+   coal-mine.problem-85))
 
 (defn run-tests []
   (clojure.test/run-tests
-    'coal-mine.problem-56
-    'coal-mine.problem-57
-    'coal-mine.problem-58
-    'coal-mine.problem-59
-    'coal-mine.problem-60
-    'coal-mine.problem-61
-    'coal-mine.problem-62
-    'coal-mine.problem-63
-    'coal-mine.problem-64
-    'coal-mine.problem-65
-    'coal-mine.problem-66
-    'coal-mine.problem-67
-    'coal-mine.problem-68
-    'coal-mine.problem-69
-    'coal-mine.problem-70
-    'coal-mine.problem-71
-    'coal-mine.problem-72
-    'coal-mine.problem-73))
+    'coal-mine.problem-74
+    'coal-mine.problem-75
+    'coal-mine.problem-76
+    'coal-mine.problem-77
+    'coal-mine.problem-78
+    'coal-mine.problem-79
+    'coal-mine.problem-80
+    'coal-mine.problem-81
+    'coal-mine.problem-82
+    'coal-mine.problem-83
+    'coal-mine.problem-84
+    'coal-mine.problem-85))
 
 (defn -main []
   (run-tests))
--- src/coal_mine/test_runner_3.cljs
+++ src/coal_mine/test_runner_3.cljs
@@ -2,18 +2,6 @@
   (:require
    [cljs.core.specs.alpha]
    [clojure.test]
-   coal-mine.problem-74
-   coal-mine.problem-75
-   coal-mine.problem-76
-   coal-mine.problem-77
-   coal-mine.problem-78
-   coal-mine.problem-79
-   coal-mine.problem-80
-   coal-mine.problem-81
-   coal-mine.problem-82
-   coal-mine.problem-83
-   coal-mine.problem-84
-   coal-mine.problem-85
    coal-mine.problem-86
    coal-mine.problem-88
    coal-mine.problem-89
@@ -27,18 +15,6 @@
 
 (defn run-tests []
   (clojure.test/run-tests
-    'coal-mine.problem-74
-    'coal-mine.problem-75
-    'coal-mine.problem-76
-    'coal-mine.problem-77
-    'coal-mine.problem-78
-    'coal-mine.problem-79
-    'coal-mine.problem-80
-    'coal-mine.problem-81
-    'coal-mine.problem-82
-    'coal-mine.problem-83
-    'coal-mine.problem-84
-    'coal-mine.problem-85
     'coal-mine.problem-86
     'coal-mine.problem-88
     'coal-mine.problem-89
